e1bdcc736c8e3d445a8be5ce561fdf950b8fa88a
Sarawanan <sarawanan.mahalingam@icloud.com>
2022-03-21 17:48:34 +0530

    Reformat the code

--- src/main/java/com/boot/webjava/repository/CartRepository.java
+++ src/main/java/com/boot/webjava/repository/CartRepository.java
@@ -5,8 +5,6 @@ import com.boot.webjava.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
     Cart findByUser(User user);
